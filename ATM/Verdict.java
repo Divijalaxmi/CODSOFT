e3928e5820c79acdf0e88927416874f4d7672ef9
Divijalaxmi <161947403+Divijalaxmi@users.noreply.github.com>
2024-06-08 13:18:34 +0530

    Create Verdict.java

---
+++ ATM/Verdict.java
@@ -0,0 +1,18 @@
+public enum Verdict {
+    SUCCESS("Success"),
+    FAILURE("Failure"),
+    OUT_OF_MONEY("Out of Money"),
+    SOFTWARE_ERROR("Software Error"),
+    HARDWARE_ERROR("Hardware Error"),
+    MAX_LIMIT("Exceed maximum withdrawal limit");
+
+    private String val;
+
+    Verdict(String val){
+        this.val = val;
+    }
+
+    public String getVal(){
+        return this.val;
+    }
+}
